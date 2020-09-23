65d90991582b3d2be01f81a2ed6942483092f8cc
Andy He <2021ahe@ras2.csl.tjhsst.edu>
2020-09-23 12:53:53 -0400

    fixed rvalues

--- Lab01/lab01.cpp
+++ Lab01/lab01.cpp
@@ -137,7 +137,8 @@ int main(int argc, char** argv){
     //euler line
     double slope = (ninepoint[1] - circum[1]) / (ninepoint[0] - circum[0]);
     double y_int = ninepoint[1] - slope * ninepoint[0];
-    drawLine(0, round(y_int), SIZE, round(slope * SIZE + y_int), vector<int> {rand()%255, rand()%255, rand()%255});
+    vector<int> color2 {rand() % 255, rand() % 255, rand() % 255};
+    drawLine(0, round(y_int), SIZE, round(slope * SIZE + y_int), color2);
     
     draw();
     cout << "done" << endl;
